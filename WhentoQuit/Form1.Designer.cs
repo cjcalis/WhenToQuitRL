2f22ed1d89869b2ddeb535eb43f191349a5cfd37
cjcalis <clinton_mcalister@yahoo.com>
2019-02-28 18:05:12 -0600

    up

--- WhentoQuit/Form1.Designer.cs
+++ WhentoQuit/Form1.Designer.cs
@@ -28,13 +28,198 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblMid = new System.Windows.Forms.Label();
+            this.lblLosses = new System.Windows.Forms.Label();
+            this.lblWins = new System.Windows.Forms.Label();
+            this.lblLoss = new System.Windows.Forms.Label();
+            this.lblWin = new System.Windows.Forms.Label();
+            this.lblLeft = new System.Windows.Forms.Label();
+            this.lblClose = new System.Windows.Forms.Label();
+            this.txtStreak = new System.Windows.Forms.TextBox();
+            this.txtStart = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(94, 202);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(182, 138);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Win Streak to Gain Life";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(182, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Starting Lives";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblMid);
+            this.panel1.Controls.Add(this.lblLosses);
+            this.panel1.Controls.Add(this.lblWins);
+            this.panel1.Controls.Add(this.lblLoss);
+            this.panel1.Controls.Add(this.lblWin);
+            this.panel1.Controls.Add(this.lblLeft);
+            this.panel1.Location = new System.Drawing.Point(38, 35);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 263);
+            this.panel1.TabIndex = 5;
+            this.panel1.Visible = false;
+            // 
+            // lblMid
+            // 
+            this.lblMid.AutoSize = true;
+            this.lblMid.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMid.ForeColor = System.Drawing.Color.White;
+            this.lblMid.Location = new System.Drawing.Point(198, 229);
+            this.lblMid.Name = "lblMid";
+            this.lblMid.Size = new System.Drawing.Size(19, 25);
+            this.lblMid.TabIndex = 5;
+            this.lblMid.Text = ":";
+            // 
+            // lblLosses
+            // 
+            this.lblLosses.AutoSize = true;
+            this.lblLosses.ForeColor = System.Drawing.Color.White;
+            this.lblLosses.Location = new System.Drawing.Point(239, 235);
+            this.lblLosses.Name = "lblLosses";
+            this.lblLosses.Size = new System.Drawing.Size(13, 13);
+            this.lblLosses.TabIndex = 4;
+            this.lblLosses.Text = "0";
+            // 
+            // lblWins
+            // 
+            this.lblWins.AutoSize = true;
+            this.lblWins.ForeColor = System.Drawing.Color.White;
+            this.lblWins.Location = new System.Drawing.Point(163, 235);
+            this.lblWins.Name = "lblWins";
+            this.lblWins.Size = new System.Drawing.Size(13, 13);
+            this.lblWins.TabIndex = 3;
+            this.lblWins.Text = "0";
+            // 
+            // lblLoss
+            // 
+            this.lblLoss.AutoSize = true;
+            this.lblLoss.BackColor = System.Drawing.Color.Red;
+            this.lblLoss.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLoss.ForeColor = System.Drawing.Color.Black;
+            this.lblLoss.Location = new System.Drawing.Point(274, 230);
+            this.lblLoss.Name = "lblLoss";
+            this.lblLoss.Size = new System.Drawing.Size(48, 22);
+            this.lblLoss.TabIndex = 2;
+            this.lblLoss.Text = "Loss";
+            this.lblLoss.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblLoss.Click += new System.EventHandler(this.lblLoss_Click);
+            // 
+            // lblWin
+            // 
+            this.lblWin.BackColor = System.Drawing.Color.Lime;
+            this.lblWin.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWin.ForeColor = System.Drawing.Color.Black;
+            this.lblWin.Location = new System.Drawing.Point(93, 230);
+            this.lblWin.Name = "lblWin";
+            this.lblWin.Size = new System.Drawing.Size(48, 22);
+            this.lblWin.TabIndex = 1;
+            this.lblWin.Text = "Win";
+            this.lblWin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblWin.Click += new System.EventHandler(this.lblWin_Click);
+            // 
+            // lblLeft
+            // 
+            this.lblLeft.AutoSize = true;
+            this.lblLeft.ForeColor = System.Drawing.Color.White;
+            this.lblLeft.Location = new System.Drawing.Point(179, 62);
+            this.lblLeft.Name = "lblLeft";
+            this.lblLeft.Size = new System.Drawing.Size(0, 13);
+            this.lblLeft.TabIndex = 0;
+            // 
+            // lblClose
+            // 
+            this.lblClose.AutoSize = true;
+            this.lblClose.BackColor = System.Drawing.Color.Red;
+            this.lblClose.Location = new System.Drawing.Point(443, 9);
+            this.lblClose.Name = "lblClose";
+            this.lblClose.Size = new System.Drawing.Size(14, 13);
+            this.lblClose.TabIndex = 6;
+            this.lblClose.Text = "X";
+            this.lblClose.Click += new System.EventHandler(this.lblClose_Click);
+            this.lblClose.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lblClose_MouseDown);
+            this.lblClose.MouseUp += new System.Windows.Forms.MouseEventHandler(this.lblClose_MouseUp);
+            // 
+            // txtStreak
+            // 
+            this.txtStreak.Location = new System.Drawing.Point(76, 135);
+            this.txtStreak.Name = "txtStreak";
+            this.txtStreak.Size = new System.Drawing.Size(100, 20);
+            this.txtStreak.TabIndex = 1;
+            // 
+            // txtStart
+            // 
+            this.txtStart.Location = new System.Drawing.Point(76, 50);
+            this.txtStart.Name = "txtStart";
+            this.txtStart.Size = new System.Drawing.Size(100, 20);
+            this.txtStart.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(469, 310);
+            this.Controls.Add(this.lblClose);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtStreak);
+            this.Controls.Add(this.txtStart);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblLeft;
+        private System.Windows.Forms.Label lblLoss;
+        private System.Windows.Forms.Label lblWin;
+        private System.Windows.Forms.Label lblMid;
+        private System.Windows.Forms.Label lblLosses;
+        private System.Windows.Forms.Label lblWins;
+        private System.Windows.Forms.Label lblClose;
+        private System.Windows.Forms.TextBox txtStreak;
+        private System.Windows.Forms.TextBox txtStart;
     }
 }
 
--- WhentoQuit/Form1.cs
+++ WhentoQuit/Form1.cs
@@ -11,10 +11,63 @@ using System.Windows.Forms;
 namespace WhentoQuit
 {
     public partial class Form1 : Form
-    {
+    {      
+              
         public Form1()
         {
             InitializeComponent();
+                        
+        }
+
+        public void btnSave_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            lblLeft.Text = txtStart.Text;
+        }
+
+        private int losses = 0;
+        public int curr = 3;
+        
+        public void lblLoss_Click(object sender, EventArgs e)
+        {            
+            losses++;
+            lblLosses.Text = losses.ToString();
+
+            curr--;
+            lblLeft.Text = curr.ToString();
+        }
+
+        private int wins = 0;
+        private int streak = 0;
+
+        public void lblWin_Click(object sender, EventArgs e)
+        {    
+            
+            wins++;
+            lblWins.Text = wins.ToString();
+            curr++;
+            streak++;
+            if(streak == 2)
+            {
+                lblLeft.Text = curr.ToString();
+            }
+
+            
+        }
+
+        private void lblClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void lblClose_MouseDown(object sender, MouseEventArgs e)
+        {
+            lblClose.BackColor = System.Drawing.Color.Tomato;
+        }
+
+        private void lblClose_MouseUp(object sender, MouseEventArgs e)
+        {
+            lblClose.BackColor = System.Drawing.Color.Red;
         }
     }
 }
